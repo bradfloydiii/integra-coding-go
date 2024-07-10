7421cf846e66074a75a66534a10dfe875713d009
Bradley Floyd III <bfloydiii@protonmail.com>
2024-07-09 21:59:30 -0400

    first commit

---
+++ server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func createUser(c echo.Context) error {
+	firstName := c.FormValue("firstName")
+	lastName := c.FormValue("lastName")
+	email := c.FormValue("email")
+	company := c.FormValue("company")
+	phone := c.FormValue("phone")
+	return c.String(http.StatusOK, "firstName: "+firstName+", lastName: "+lastName+", email: "+email+", company: "+company+", phone: "+phone)
+}
+
+type User struct {
+	ID        int    `json:"_id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Company   string `json:"company"`
+	Phone     string `json:"phone"`
+}
+
+func getUsers(c echo.Context) error {
+	users := []User{
+		{ID: 1, FirstName: "John", LastName: "Doe", Email: "john@example.com", Company: "ABC Inc.", Phone: "1234567890"},
+		{ID: 2, FirstName: "Jane", LastName: "Smith", Email: "jane@example.com", Company: "XYZ Corp.", Phone: "9876543210"},
+		// Add more users as needed
+	}
+
+	return c.JSON(http.StatusOK, users)
+}
+
+func updateUser(c echo.Context) error {
+	id := c.Param("id")
+	firstName := c.FormValue("firstName")
+	lastName := c.FormValue("lastName")
+	email := c.FormValue("email")
+	company := c.FormValue("company")
+	phone := c.FormValue("phone")
+	return c.String(http.StatusOK, "id: "+id+", firstName: "+firstName+", lastName: "+lastName+", email: "+email+", company: "+company+", phone: "+phone)
+}
+
+func deleteUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "Deletes user in database with id="+id)
+}
+
+func main() {
+	app := echo.New() // Create a new echo instance
+	port := ":1323"   // Port number
+
+	// Routes
+	app.POST("/user", createUser)
+	app.GET("/user", getUsers)
+	app.PUT("/user/:id", updateUser)
+	app.DELETE("/user/:id", deleteUser)
+
+	// Start server
+	app.Logger.Fatal(app.Start(port))
+}
