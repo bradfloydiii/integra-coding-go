ced69b9a467e36ca5ab4cb0724e98224b386b256
Bradley Floyd III <bfloydiii@protonmail.com>
2024-07-11 01:47:31 -0400

    connecting backend to database

--- server.go
+++ server.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
@@ -21,63 +21,64 @@ type User struct {
 	Phone     string `json:"phone"`
 }
 
-type UserDB struct {
-	ID        int    `db:"id"`
-	FirstName string `db:"firstName"`
-	LastName  string `db:"lastName"`
-	Email     string `db:"email"`
-	Company   string `db:"company"`
-	Phone     string `db:"phone"`
+var db *sql.DB
+
+func main() {
+	app := echo.New()          // Create a new echo instance
+	app.Use(middleware.CORS()) // Enable CORS
+
+	port := ":1323"  // Port number
+	version := "/v1" // API version
+
+	// connect to the postgres database
+	var err error
+	db, err = sql.Open("postgres", "user=postgres password=password dbname=users port=5432 sslmode=disable") // would separate this into a config file
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// test the connection to the database
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("database connected successfully")
+	}
+
+	app.GET(version+"/user", getUsers)
+	app.POST(version+"/user", createUser)
+	app.PUT(version+"/user/:id", updateUser)
+	app.DELETE(version+"/user/:id", deleteUser)
+
+	app.Logger.Fatal(app.Start(port))
 }
-
-// func connection() {
-
-// 	// //connect to a PostgreSQL database
-// 	// // Replace the connection details (user, dbname, password, host) with your own
-// 	// db, err := sqlx.Connect("postgres", "user=postgres dbname=users sslmode=disable password=password host=localhost")
-// 	// if err != nil {
-// 	// 	log.Fatalln(err)
-// 	// }
-
-// 	// defer db.Close()
-
-// 	// // Test the connection to the database
-// 	// if err := db.Ping(); err != nil {
-// 	// 	log.Fatal(err)
-// 	// } else {
-// 	// 	log.Println("Successfully connected to PostgreSQL database")
-// 	// }
-
-// 	// place := User{} // Initialize a User struct to hold retrieved data
-
-// 	// // Execute a SQL query to select "username" and "email" columns from the "users" table
-// 	// rows, _ := db.Queryx("SELECT * FROM users")
-
-// 	// for rows.Next() {
-// 	// 	err := rows.StructScan(&place) // Scan the current row into the "place" variable
-// 	// 	if err != nil {
-// 	// 		log.Fatalln(err)
-// 	// 	}
-// 	// 	log.Printf("%#v\n", place) // Log the content of the "place" struct for each row
-// 	// }
-// }
 
 // swagger:route GET /v1/user users getUsers
 // Get a list of users.
 // responses:
-//   200: usersResponse
-
-// getUsers handles the GET request for retrieving a list of users.
+//
+//	200: usersResponse
 func getUsers(c echo.Context) error {
+	c.Response().Header().Set("Content-Type", "application/json")
 
-	users := []User{
-		{ID: 1, FirstName: "John", LastName: "Doe", Email: "john@example.com", Company: "ABC Inc.", Phone: "1234567890"},
-		{ID: 2, FirstName: "Jane", LastName: "Smith", Email: "jane@example.com", Company: "XYZ Corp.", Phone: "9876543210"},
-		// Add more users as needed
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer rows.Close()
 
-	// users := sq.Select("*").From("users").Join("emails USING (email_id)")
-	// row := sqlx.DB.QueryRow("SELECT * FROM users")
+	var users []User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Company, &user.Phone)
+		if err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, user)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return c.JSON(http.StatusOK, users)
 }
@@ -85,20 +86,32 @@ func getUsers(c echo.Context) error {
 // swagger:route POST /v1/user users createUser
 // Create a new user.
 // responses:
-//   200: userResponse
-
+//
+//	200: userResponse
+//
 // createUser handles the POST request for creating a new user.
 func createUser(c echo.Context) error {
-	firstName := c.FormValue("firstName")
-	lastName := c.FormValue("lastName")
+
+	firstname := c.FormValue("firstname")
+	lastname := c.FormValue("lastname")
 	email := c.FormValue("email")
 	company := c.FormValue("company")
 	phone := c.FormValue("phone")
 
-	fmt.Println(http.StatusOK, "firstName: "+firstName+", lastName: "+lastName+", email: "+email+", company: "+company+", phone: "+phone)
-	// row := sqlx.DB.QueryRow("INSERT INTO users (first_name, last_name, email, company, phone) VALUES ($1, $2, $3, $4, $5)", firstName, lastName, email, company, phone)
+	fmt.Println("firstname: " + firstname + ", lastname: " + lastname + ", email: " + email + ", company: " + company + ", phone: " + phone)
 
-	return c.JSON(http.StatusOK, []User{})
+	stmt, err := db.Prepare("INSERT INTO users (firstname, lastname, email, company, phone) VALUES ($1, $2, $3, $4, $5)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if _, err := stmt.Exec(firstname, lastname, email, company, phone); err != nil {
+		log.Fatal(err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "User NOT created successfully"})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "User created successfully"})
 }
 
 // swagger:route PUT /v1/user/{id} users updateUser
@@ -109,16 +122,26 @@ func createUser(c echo.Context) error {
 // updateUser handles the PUT request for updating an existing user.
 func updateUser(c echo.Context) error {
 	id := c.Param("id")
-	firstName := c.FormValue("firstName")
-	lastName := c.FormValue("lastName")
+	firstname := c.FormValue("firstname")
+	lastname := c.FormValue("lastname")
 	email := c.FormValue("email")
 	company := c.FormValue("company")
 	phone := c.FormValue("phone")
 
-	fmt.Println(http.StatusOK, "id: "+id+", firstName: "+firstName+", lastName: "+lastName+", email: "+email+", company: "+company+", phone: "+phone)
-	// row := sqlx.DB.QueryRow("UPDATE users SET first_name=$1, last_name=$2, email=$3, company=$4, phone=$5 WHERE id=$6", firstName, lastName, email, company, phone, id)
+	fmt.Println("id: " + id + ", firstname: " + firstname + ", lastname: " + lastname + ", email: " + email + ", company: " + company + ", phone: " + phone)
 
-	return c.JSON(http.StatusOK, []User{})
+	stmt, err := db.Prepare("UPDATE users SET firstname=$1, lastname=$2, email=$3, company=$4, phone=$5 WHERE id=$6")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if _, err := stmt.Exec(firstname, lastname, email, company, phone, id); err != nil {
+		log.Fatal(err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "User NOT updated successfully"})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "User created successfully"})
 }
 
 // swagger:route DELETE /v1/user/{id} users deleteUser
@@ -129,39 +152,17 @@ func updateUser(c echo.Context) error {
 // deleteUser handles the DELETE request for deleting a user.
 func deleteUser(c echo.Context) error {
 	id := c.Param("id")
-	// row := sqlx.DB.QueryRow("DELETE FROM users WHERE id=$1", id)
 
-	return c.JSON(http.StatusOK, "Deleted user in database with id="+id)
-}
+	fmt.Println("id: " + id)
 
-func main() {
-	app := echo.New() // Create a new echo instance
-
-	app.Use(middleware.CORS()) // Enable CORS
-
-	port := ":1323"  // Port number
-	version := "/v1" // API version
-
-	//connect to the PostgreSQL database
-	db, err := sqlx.Connect("postgres", "user=postgres dbname=users sslmode=disable password=password host=localhost")
+	stmt, err := db.Prepare("DELETE FROM users WHERE id=$1")
 	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
-
-	// Test the connection to the database
-	if err := db.Ping(); err != nil {
 		log.Fatal(err)
-	} else {
-		log.Println("PostgreSQL connected successfully")
+	}
+	if _, err := stmt.Exec(id); err != nil {
+		log.Fatal(err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "User NOT deleted successfully"})
 	}
 
-	// routes
-	app.POST(version+"/user", createUser)
-	app.GET(version+"/user", getUsers)
-	app.PUT(version+"/user/:id", updateUser)
-	app.DELETE(version+"/user/:id", deleteUser)
-
-	// start server
-	app.Logger.Fatal(app.Start(port))
+	return c.JSON(http.StatusOK, map[string]string{"message": "Deleted user successfully"})
 }
